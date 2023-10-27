5becbb8859df9f148d1f6127d39d982833324bc9
Emansyed33 <144342740+Emansyed33@users.noreply.github.com>
2023-10-27 14:53:21 +0500

    variables declared

--- .vs.c
+++ .vs.c
@@ -7,46 +7,14 @@ The programme that calculate the user data and inform the user about the data wh
 #include <stdlib.h>
 int main()
 {
-    double totalGB, total_used, averageDailyuse, Max_use, Remaining_GB;
-    int currentDay, Remaining_Days;
-    printf("Number of GB in the plan per 30 day period: ");
-    scanf("%lf",&totalGB);
-    printf("The current day in the 30 day period (in the range 1,first day, for the last day) : ");
-    scanf("%d",&currentDay);
-    printf("The total number of GB used so far : ");
-    scanf("%lf",&total_used);
-    remaining_Days = 30 - CurrentDay;
-    remaining_GB = totalGB - total_used;
-    averageDailyUse = total_used /currentDay;
-    printf("The total number of GB used so far : ");
-    scanf("lf",&total_used);
-    Remaining_days = 30 - currentDay;
-    Remaining_GB = totalGB - total_used;
-    AverageDailyuse = total_used/currentDay;
-    max_use = totalGB / 30;
-    printf("%d days used, %d days remaining\n", currentDay, Remaining_Days);
-    if(averageDailyuse > Max_use)
-    {
-        printf("you are extending your average daily use (%.3If GB/Day).\n, Max_use");
-        double suggestion = (averageDailyuse - Max_Use);
-        double ExtraUse = (totalGB - total_used/Remaining_Days);
-        printf("you are able to use upto %.3lf GB/day and stay below your data plan limit\n,ExtraUse");
-        
-      } 
-      else 
-      {
-        printf("you have already availed your package for this month\n",ExtraUse):
+    int days;
+    float average_spending;
+    int remaining_days;
+    float GBs_used;
+    int GB_limit=15;
+    float end_result;
+    float exceeding_limit;
+    float daily_allowance;
+    int lim;
 
-      } 
-      return 0;
 }
-
-
-
-
-
-
-
-
-    
-    
